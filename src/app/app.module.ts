200aa9a4611460b4e8f54c6acdd676750d1a1d6b
Dheeraj Jaiswal <jdheeraj32@gmail.com>
2019-08-13 15:23:50 +0400

    Angular TemplateDriven Form Demo Done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponentComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
